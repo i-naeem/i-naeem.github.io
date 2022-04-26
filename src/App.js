47562269cd7b9a4dffd79e89b29f3282ee20a12f
i-naeem <inaeem@protonmail.com>
2022-04-26 18:05:40 +0500

    :sparkles: add project cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import { Box, HStack, Icon, Link, SimpleGrid } from '@chakra-ui/react';
+import {
+  SimpleGrid,
+  Heading,
+  HStack,
+  Image,
+  Stack,
+  Link,
+  Icon,
+  Box,
+  Text,
+} from '@chakra-ui/react';
 import { FaNeos } from 'react-icons/fa';
 
+const projects = [
+  {
+    id: 1,
+    title: 'Definion',
+    description: 'A simple dictionary to search the definition of a word.',
+    img: 'https://github.com/i-naeem/definion/raw/alpha/public/assets/demo.png',
+  },
+  {
+    id: 2,
+    title: 'Heyming',
+    description:
+      'Heyming is a Hamming Code encode which encodes given data in hamming code and decodes hamming code.',
+    img: 'https://raw.githubusercontent.com/i-naeem/heyming/alpha/demo.jpeg',
+  },
+];
 const App = (props) => {
   return (
     <SimpleGrid>
@@ -16,7 +41,29 @@ const App = (props) => {
       </header>
 
       <main>
-        <Box px={[2, 10]}>Hello World</Box>
+        <Box px={[2, 10]}>
+          <Stack height="80vh" justify="center">
+            <Heading fontSize="6rem" lineHeight="0.9" textTransform="uppercase">
+              Hi, I'm <br />
+              Mohammad Naeem<sup>*</sup>
+            </Heading>
+            <Text color="gray" as="small">
+              * ACTUALLY, I'M A WEBSITE. BUT I WAS BUILT BY LUKE MEYER.
+            </Text>
+          </Stack>
+
+          <SimpleGrid columns={3} gap={1}>
+            {projects.map((project) => {
+              return (
+                <Stack p="2">
+                  <Image src={project.img} alt={project.title} />
+                  <Heading size="md">{project.title}</Heading>
+                  <Text fontSize="sm">{project.description}</Text>
+                </Stack>
+              );
+            })}
+          </SimpleGrid>
+        </Box>
       </main>
     </SimpleGrid>
   );
